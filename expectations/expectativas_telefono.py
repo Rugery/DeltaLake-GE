2bb35c50ffd70f020c2b5ded1f5f407dd1573811
Rugery <rugeryde@gmail.com>
2025-06-27 15:39:47 -0500

    Fix min_valor in telefono length validation to allow values from 1 to 10

--- expectations/expectativas_telefono.py
+++ expectations/expectativas_telefono.py
@@ -16,6 +16,6 @@ def suite_Telefonos():
     return [
         expectativa_valores_por_referencia_caracteres(columna="telefono", referencia=ref_Telefonos, caracteres=3),
         expectativa_valores_no_nullos(columna="telefono"),
-        expectativa_valores_longitud_entre(columna="telefono", min_valor=10, max_valor=10),
+        expectativa_valores_longitud_entre(columna="telefono", min_valor=1, max_valor=10),
         expectativa_valores_longitud_igual(columna="telefono", longitud=10),
     ]
